00d2b7145cd7b451ec020d8d98c88ff77500ddb6
Navi321 <49982747+Navi321@users.noreply.github.com>
2023-11-05 17:02:28 +0200

    add header and button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import { useEffect } from 'react';
 import './App.css';
-const tg = window.Telegram.WebApp;
+
 
 function App() {
 
   useEffect( () => {
     tg.ready();
   }, [])
-
-  const onClose = () => {
-    tg.close();
-  }
 
   return (
     <div className="App">

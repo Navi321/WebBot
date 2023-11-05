c3f255464e3da6361343fe2a4c99acf18c5e4995
Navi321 <49982747+Navi321@users.noreply.github.com>
2023-11-05 17:17:27 +0200

    add Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
+import Header from './components/Header/Header'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <button onClick={onToggleButton}>toogle</button>
     </div>
   );

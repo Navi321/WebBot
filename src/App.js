8dc5c5197c89a1f98b18c7f55e2f0a2e6324a233
Navi321 <49982747+Navi321@users.noreply.github.com>
2023-11-05 17:14:44 +0200

    first hook and new button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from './hooks/useTelegram';
 
 
 function App() {
+  const {onToggleButton, tg} = useTelegram();
+
 
   useEffect( () => {
     tg.ready();
@@ -10,8 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      work
-      <button onClick={onClose}>Закрыть</button>
+      <button onClick={onToggleButton}>toogle</button>
     </div>
   );
 }

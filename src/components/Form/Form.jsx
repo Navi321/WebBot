5ec9ba8f94700bfbcf30c3028d97ffd87c50bdb8
Navi321 <49982747+Navi321@users.noreply.github.com>
2023-11-07 08:57:57 +0200

    fix sending data

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,14 +15,14 @@ const Form = () => {
             subject
         }
         tg.sendData(JSON.stringify(data));
-    }, [])
+    }, [country, city, subject])
 
     useEffect( () => {
         tg.onEvent('mainButtonClicked', onSendData)
         return () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [])
+    }, [onSendData])
 
     useEffect( () => {
         tg.MainButton.setParams({
